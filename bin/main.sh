c229e5a222e076fe56749f672696bae18fb6999e
Jeffery Fernandez <jefferyfernandez@gmail.com>
2019-05-03 23:48:09 +1000

    moved setup script into main script as a function

--- bin/main.sh
+++ bin/main.sh
@@ -10,8 +10,15 @@ function startPortal()
 
 function startSetup()
 {
-    # Run the gwsetup service in the background
-    setup.sh
+    pushd ${HOME} 1> /dev/null
+
+        if [[ -n "${HOST_IP}" ]]; then
+           echo "${HOST_IP}" > ${HOME}/gwsetup_only.txt
+        fi
+
+        gwsetup -p 2316 -gd /usr/share/geneweb -lang ${LANGUAGE} -bindir /usr/bin -only ${HOME}/gwsetup_only.txt -log /dev/null 2>&1 | tee -a ${HOME}/gwsetup.log
+
+    popd 1> /dev/null
 }
 
 function runBackup()
