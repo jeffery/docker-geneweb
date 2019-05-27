8e9d5c61b301b781e367814af3c68fda9c789e4b
Jeffery Fernandez <jefferyfernandez@gmail.com>
2019-05-27 15:31:15 +1000

    Refactored command options for building, running and stopping GeneWeb

--- main.sh
+++ main.sh
@@ -1,21 +1,60 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
-PORTAL_PORT=2317
-SETUP_PORT=2316
-DATA_HOME=~/GenealogyData
 PROJECT_NAME=jeffernz-geneweb
 PROJECT_RELEASE=0.8
+
+WEB_PORT=2317
+SETUP_PORT=2316
+DATA_HOME=~/GenealogyData
+LANGUAGE="en"
+HOST_IP="172.17.0.1"
+TIME_ZONE="Australia/Melbourne"
 
 function containerName()
 {
     echo "${PROJECT_NAME}_${PROJECT_RELEASE}"
 }
 
-function buildContainer()
+function usage()
 {
-    docker build -t jeffernz/geneweb:latest -t jeffernz/geneweb:${PROJECT_RELEASE} .
+    cat << EOF
+
+${0}
+
+Command options:
+    -h      Display this help message.
+    -b      Build the GeneWeb container
+    -r | -z Run the GeneWeb container | Build and run the GeneWeb Container
+            Option parameters:
+                -l  The language to run GeneWeb in e.g. (en, de, fr)
+                -i  The host ip address where the setup portal will be accessed from
+                -t  The time zone to run GeneWeb in. e.g. (Australia/Melbourne)
+                -w  The web portal port number. e.g. 2317
+                -s  The setup portal port number. e.g. 2316
+                -b  The storage location for the bases e.g. ~/GenealogyData
+    -k      Stop the container
+
+EOF
+
+    exit 1
+}
+
+function displayRunning()
+{
+    cat << RUNNING
+
+ GeneWeb is running using language '${LANGUAGE}' in the time zone '${TIME_ZONE}'
+
+    The bases will be stored under '${DATA_HOME}'
+
+    The web portal can be accessed at http://localhost:${WEB_PORT}
+    The setup portal can be accessed at http://localhost:${SETUP_PORT}
+
+    To stop the GeneWeb container, execute: '${0} -k'
+
+RUNNING
 }
 
 function checkoutRepo()
@@ -23,24 +62,36 @@ function checkoutRepo()
     git clone https://github.com/jeffery/docker-geneweb.git
 }
 
-function stopInstructions()
+function buildContainer()
 {
+    docker build -t jeffernz/geneweb:latest -t jeffernz/geneweb:${PROJECT_RELEASE} .
+}
 
-    cat <<EOT
-##############################################################################
-#                                                                            #
-#  Your GeneWeb docker container is now up and running.                      #
-#                                                                            #
-#    Access to the GeneWeb Portal is at http://localhost:${PORTAL_PORT}                #
-#                                                                            #
-#    Access to the Setup Portal is at http://localhost:${SETUP_PORT}                  #
-#                                                                            #
-#  To stop the docker container, execute: 'docker stop $(containerName)' #
-#                                                                            #
-##############################################################################
+function runContainer()
+{
+    # Create the database directory
+    mkdir -p ${DATA_HOME}
 
-EOT
+    # Remove any running/old containers
+    stopContainer && removeContainer
 
+    # Run the container in detached mode
+    docker run \
+        -d=true \
+        --restart unless-stopped \
+        -p ${SETUP_PORT}:2316 \
+        -p ${WEB_PORT}:2317 \
+        -v ${DATA_HOME}:/usr/local/var/geneweb/ \
+        --env HOST_IP=${HOST_IP} \
+        --env LANGUAGE=${LANGUAGE} \
+        --env TZ=${TIME_ZONE} \
+        --name $(containerName) \
+        jeffernz/geneweb:latest
+}
+
+function stopContainer()
+{
+    docker stop $(containerName)
 }
 
 function removeContainer()
@@ -50,58 +101,65 @@ function removeContainer()
     set -e
 }
 
-function runContainer()
+function invalidOption()
 {
-    # Create the database directory
-    mkdir -p ${DATA_HOME}
-
-    # Remove any running/old containers
-    removeContainer
-
-    # Run the container in detached mode
-    docker run \
-    -d=true \
-    --restart unless-stopped \
-    -p ${SETUP_PORT}:2316 \
-    -p ${PORTAL_PORT}:2317 \
-    -v ${DATA_HOME}:/usr/local/var/geneweb/ \
-    --env HOST_IP=172.17.0.1 \
-    --env LANGUAGE=en \
-    --env TZ=Australia/Melbourne \
-    --name $(containerName) \
-    jeffernz/geneweb:latest && stopInstructions
+    echo "Invalid Command: -$OPTARG" 1>&2
 }
 
-function stopContainer()
+function invalidOptionParameter()
 {
-    docker stop $(containerName)
+    echo "Invalid Option: -$OPTARG" 1>&2
 }
 
-case "$1" in
-        build)
-            buildContainer
-            ;;
+function optionParameterRequiresValue()
+{
+    echo "Option: -$OPTARG requires an argument" 1>&2
+}
 
-        run)
+while getopts ":hbrzk" opt; do
+  case ${opt} in
+
+    h ) usage ;;
+
+    b ) buildContainer; exit $? ;;
+
+    r | z )
+        while getopts ":l:t:i:w:s:b:" opt; do
+            case ${opt} in
+                l ) LANGUAGE=$OPTARG ;;
+
+                t ) TIME_ZONE=$OPTARG ;;
+
+                i ) HOST_IP=$OPTARG ;;
+
+                w ) WEB_PORT=$OPTARG ;;
+
+                s ) SETUP_PORT=$OPTARG ;;
+
+                b ) DATA_HOME=$OPTARG ;;
+
+                \? ) invalidOptionParameter && usage ;;
+
+                : ) optionParameterRequiresValue && usage ;;
+            esac
+        done
+
+        if [[ ${opt} -eq "r" ]]; then
             runContainer
-            ;;
+        else
+            buildContainer && runContainer
+        fi
 
-        stop)
-            stopContainer
-            ;;
+        displayRunning; exit $?
+    ;;
 
-        build-run)
-            buildContainer && stopContainer && runContainer
-            ;;
+    k ) stopContainer; exit $? ;;
 
-        bootstrap)
-            checkoutRepo && pushd docker-geneweb 1> /dev/null && buildContainer && popd 1> /dev/null && runContainer
-            ;;
+    \? ) invalidOption && usage ;;
 
-        *)
-            echo $"Usage: $0 {build|run|build-run|bootstrap|stop}"
-            exit 1
+  esac
+done
 
-esac
-
-
+if [[ $OPTIND -eq 1 ]]; then
+    usage
+fi
