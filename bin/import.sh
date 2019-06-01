51c7b8af0d7fc1348201dd4724f459eefc509aef
Jeffery Fernandez <jefferyfernandez@gmail.com>
2019-06-01 12:00:31 +1000

    Adding import script

---
+++ bin/import.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+pushd ${HOME} 1> /dev/null
+
+OLD_IFS=${IFS}
+IFS="
+"
+    for ged in `ls -f import/*.ged 2> /dev/null`;
+    do
+        gedComFile=$(basename ${ged})
+        baseName=$(echo $(echo "${gedComFile%.*}") | tr -cd '[:alnum:]-')
+
+        echo "# Importing GEDCOM file '${PWD}/${ged}' as '${baseName}'" | tee -a backup.log
+        ged2gwb2 -f "${PWD}/${ged}" -o ${baseName} 2>&1 | tee -a import.log
+    done;
+
+IFS=${OLD_IFS}
+
+popd 1> /dev/null
